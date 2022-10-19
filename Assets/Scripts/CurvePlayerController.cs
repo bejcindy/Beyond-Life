c2016d277596274c2d121161ee77696085d194e1
Cindy Fan <fanj696@newschool.edu>
2022-10-19 00:10:44 -0400

    imported TMP and fixed gravity; added R to refresh level

--- Assets/Scripts/CurvePlayerController.cs
+++ Assets/Scripts/CurvePlayerController.cs
@@ -9,11 +9,14 @@ public class CurvePlayerController : MonoBehaviour
     float rotationX;
     public float lookSpeed = 5.0f;
     public float lookXLimit = 45.0f;
-
+    public float g;
+    public Transform[] legPos;
     public Transform cam;
 
+    bool[] groundCheck;
     Rigidbody rb;
 
+    bool offground;
     bool parallelToGround;
     Vector3 currentDirection;
     Vector3 zeroX, zeroY, zeroZ;
@@ -28,13 +31,16 @@ public class CurvePlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         parallelToGround = true;
+        //legPos = new Transform[4];
+        groundCheck = new bool[4];
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
         Vector3 direction = (transform.forward * z + transform.right * x).normalized;
         //zeroY = new Vector3(direction.x, 0, direction.z);
         //zeroZ = new Vector3(direction.x, direction.y, 0);
@@ -42,32 +48,39 @@ public class CurvePlayerController : MonoBehaviour
 
         rb.velocity = direction * speed;
 
-        //transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-        //transform.LookAt(mouseWorldSpace, realUp);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 3))
+        {
+            Physics.gravity = hit.normal * g;
+            offground = false;
+        }
 
-        //forward
-        //Vector3 newUp;
-        //RaycastHit hit;
-        //if (Physics.Raycast(transform.position, -transform.up, out hit, 3))
-        //{
-        //    //transform.rotation *= (Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0) * Quaternion.FromToRotation(transform.up, hit.normal));
-        //    //transform.rotation *= Quaternion.FromToRotation(transform.up, hit.normal);
-        //    //transform.Rotate(Quaternion.FromToRotation(transform.up, hit.normal).eulerAngles.x, 0, Quaternion.FromToRotation(transform.up, hit.normal).eulerAngles.z);
-        //    newUp = hit.normal;
-        //    Vector3 left = Vector3.Cross(transform.forward, newUp);
-        //    Vector3 newForward = Vector3.Cross(newUp, left);
-        //    Quaternion newRotation = Quaternion.LookRotation(newForward, newUp);
-        //    transform.rotation = newRotation;
-        //    //Debug.Log(hit.normal);
-        //    //realUp = (hit.point - transform.position).normalized;
-        //    //Debug.DrawRay(transform.position, -transform.right*3f, Color.red);
-        //}
+        for (int i = 0; i < legPos.Length; i++)
+        {
+            RaycastHit h;
+            if (Physics.Raycast(legPos[i].position, -transform.up, out h, 3))
+            {
+                Physics.gravity = h.normal * g;
+                groundCheck[i] = true;
+            }
+            else
+            {
+                groundCheck[i] = false;
+            }
+        }
+        Debug.DrawRay(legPos[0].position, -transform.up, Color.blue);
+        Debug.Log(groundCheck[0] + " " + groundCheck[1] + " " + groundCheck[2] + " " + groundCheck[3] + " ");
+        if (!groundCheck[0] && !groundCheck[1] && !groundCheck[2] && !groundCheck[3])
+        {
+            rb.AddForce(-transform.up * -g * 4000 * Time.deltaTime, ForceMode.Acceleration);
+            Debug.Log("here");
+        }
         
+
         transform.Rotate(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-
-
     }
 
+  
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Curve"))
@@ -85,7 +98,7 @@ public class CurvePlayerController : MonoBehaviour
             if (Physics.Raycast(transform.position, -transform.up, out hit, 3))
             {
                 //Physics.gravity = (transform.position - hit.point).normalized * -9.81f;
-                Physics.gravity = hit.normal * -9.81f;
+                //Physics.gravity = hit.normal * -9.81f;
                 //transform.rotation *= Quaternion.FromToRotation(transform.up, hit.normal);
                 //transform.up += hit.normal;
                 newUp = hit.normal;

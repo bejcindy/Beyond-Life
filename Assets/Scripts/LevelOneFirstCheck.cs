727bb9cbf56929a6e476d9fa390e016a237fd278
Luke <yl4107@nyu.edu>
2022-11-15 02:15:31 -0500

    temp save

--- Assets/Scripts/LevelOneFirstCheck.cs
+++ Assets/Scripts/LevelOneFirstCheck.cs
@@ -36,6 +36,8 @@ public class LevelOneFirstCheck   : MonoBehaviour
     public bool failedFirstCheck = false;
     public bool failedSecondCheck = false;
 
+    Animator tunnelAnim;
+
     [SerializeField] Material greenMat;
     [SerializeField] Material yellowMat;
     [SerializeField] Material redMat;
@@ -50,23 +52,13 @@ public class LevelOneFirstCheck   : MonoBehaviour
         //pressCDVal = pressCD;
         AS = GetComponent<AudioSource>();
         currentMat = greenMat;
+
+        tunnelAnim = myTunnel.transform.parent.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //if (Input.anyKey)
-        //{
-        //    if (Input.GetKey(KeyCode.E))
-        //    {
-        //        Debug.Log("pressed E");
-        //    }
-        //    else
-        //    {
-        //        Debug.Log("wrong key");
-        //    }
-        //}
 
         if (inCheck)
         {
@@ -79,41 +71,71 @@ public class LevelOneFirstCheck   : MonoBehaviour
 
         if (inSecondCheck)
         {
-            holdTime -= Time.deltaTime * holdTimeSpeed;
-            holdFailCountDown -= Time.deltaTime;
+            
+            //holdFailCountDown -= Time.deltaTime;
 
-            if (holdFailCountDown <= 0)
-            {
-                if (!Input.GetKey(KeyCode.E))
-                {
-                    failedSecondCheck = true;
-                }
+            //if (holdFailCountDown <= 0)
+            //{
+            //    if (!Input.GetKey(KeyCode.E))
+            //    {
+            //        failedSecondCheck = true;
+            //    }
 
-            }
+            //}
             if (Input.GetKey(KeyCode.E))
             {
+                holdTime -= Time.deltaTime * holdTimeSpeed;
                 Debug.Log("pressing and holding E");
             }
 
             if (Input.GetKeyUp(KeyCode.E))
             {
-                Debug.Log("you failed second check");
-                failedSecondCheck = true;
+                if(holdTime > 0)
+                {
+                    Debug.Log("you failed second check");
+                    failedSecondCheck = true;
+                }
+
             }
 
-            if(holdTime <= 0)
+            if(tunnelAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
             {
-                if (failedSecondCheck)
+                if(holdTime <= 0)
                 {
-                    triggerDropPlayer();
+                    if (failedSecondCheck)
+                    {
+                        triggerDropPlayer();
+                    }
+                    else
+                    {
+                        resumePlayer();
+                    }
                 }
                 else
                 {
-                    resumePlayer();
+                    failedSecondCheck = true;
+                    triggerDropPlayer();
                 }
-                inSecondCheck = false;
                 checkKey.SetActive(false);
             }
+
+
+            //if(holdTime <= 0)
+            //{
+            //    if (failedSecondCheck)
+            //    {
+            //        triggerDropPlayer();
+            //    }
+            //    else
+            //    {
+            //        if (tunnelAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            //        {
+            //            resumePlayer();
+            //        }
+                        
+            //    }
+            //    checkKey.SetActive(false);
+            //}
         }
 
 
@@ -142,11 +164,11 @@ public class LevelOneFirstCheck   : MonoBehaviour
             {
                 failedFirstCheck = true;
             }
-            enterSecondCheck();
+            StartCoroutine(enterSecondCheck());
         }
     }
 
-    public void enterSecondCheck()
+    public IEnumerator enterSecondCheck()
     {
         if (failedFirstCheck)
         {
@@ -154,12 +176,15 @@ public class LevelOneFirstCheck   : MonoBehaviour
             mainTrack.GetComponent<Animator>().speed = 0;
             playerTile.SetActive(false);
             player.transform.parent = myTunnel.transform;
-            //player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
-            myTunnel.GetComponent<Animator>().enabled = true;
+            player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+            yield return new WaitForSeconds(1.0f);
+            tunnelAnim.enabled = true;
             inSecondCheck = true;
             checkKey.SetActive(true);
         }
     }
+
+
 
     void triggerDropPlayer()
     {
@@ -168,8 +193,20 @@ public class LevelOneFirstCheck   : MonoBehaviour
 
     void resumePlayer()
     {
+
+        Debug.Log("done rotating");
+        inSecondCheck = false;
         player.transform.parent = mainTrack.transform;
+        player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | 
+                                                        RigidbodyConstraints.FreezeRotationZ | 
+                                                        RigidbodyConstraints.FreezeRotationY |
+                                                        RigidbodyConstraints.FreezePositionY;
+        playerTile.SetActive(true);
         mainTrack.GetComponent<Animator>().speed = 1;
+        
+
+
+
     }
     IEnumerator resetDoor()
     {
--- Assets/StartPanel.cs
+++ Assets/StartPanel.cs
@@ -9,26 +9,84 @@ public class StartPanel : MonoBehaviour
 
     public AudioClip buttonSound;
 
+    public GameObject mainTrack;
+    public AudioSource ambienceAS;
+
+    public float pressWaitTime = 3.0f;
+    public float pressWaitTimeVal;
+
+    public bool inButtonWait = false;
+    public bool inCheck = false;
+
+    public int pressAmt = 0;
+
     AudioSource AS;
+    Animator buttonAnim;
     void Start()
     {
         startButton = transform.GetChild(0).gameObject;
+        buttonAnim = startButton.GetComponent<Animator>();
         AS = gameObject.GetComponent<AudioSource>();
+
+        pressWaitTimeVal = pressWaitTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (inCheck)
         {
-            GetComponent<Animator>().enabled = true;
-            startButton.GetComponent<Animator>().SetBool("FirstPress", true);
-            AS.PlayOneShot(buttonSound);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponent<Animator>().enabled = true;
+                buttonAnim.SetBool("FirstPress", true);
+                
+                AS.PlayOneShot(buttonSound);
+            }
+
+
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                buttonAnim.SetBool("ButtonUp", true);
+                inButtonWait = true;
+                inCheck = false;
+                pressAmt += 1;
+            }
         }
 
-        if (Input.GetKeyUp(KeyCode.E))
+
+        if (inButtonWait)
         {
-            startButton.GetComponent<Animator>().SetBool("ButtonUp", true);
+            StartCoroutine(restartButton());
+
         }
+
+        switch (pressAmt)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                mainTrack.GetComponent<AudioSource>().volume += 0.1f * Time.deltaTime;
+                break;
+            default:
+                ambienceAS.volume += 0.1f * Time.deltaTime;
+                break;
+        }
+
+        
+    }
+
+    IEnumerator restartButton()
+    {
+        inButtonWait = false;
+        yield return new WaitForSeconds(5.0f);
+        buttonAnim.SetBool("FirstPress", false);
+        buttonAnim.SetBool("ButtonUp", false);
+        buttonAnim.SetBool("Reset", true);
+        yield return new WaitForSeconds(2.0f);
+        buttonAnim.SetBool("Reset", false);
+        inCheck = true;
     }
 }

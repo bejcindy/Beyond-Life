d6132465d8f20078955b64a969f54e5acfa2146d
egfus <yw5910@nyu.edu>
2022-11-17 17:09:35 -0500

    top ring shader

---
+++ Assets/shader/top_ring_gradient.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class top_ring_gradient : MonoBehaviour
+{
+    [ColorUsage(true, true)]
+    [SerializeField] Color color_1;
+
+    [ColorUsage(true, true)]
+    [SerializeField] Color color_2;
+
+    [SerializeField] MeshRenderer[] CubeMat;
+
+    public float smoothness = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for ( int i = 0; i < CubeMat.Length; i++)
+        {
+            smoothness += 1f / CubeMat.Length;
+            CubeMat[i].material.color = Color.Lerp(color_1, color_2, smoothness);  
+        }
+             
+    }
+
+    
+}
